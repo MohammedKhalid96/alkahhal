bc4d0591552b79396181d6d66383e4512895ac5e
MohammedKhalid96 <mohammedkhalid96@outlook.com>
2020-07-25 01:00:32 +0200

    run heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-let port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
