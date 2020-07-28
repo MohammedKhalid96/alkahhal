d8f941503be5a8823b16a12a36a3525a77d3773f
MohammedKhalid96 <mohammedkhalid96@outlook.com>
2020-07-28 14:31:43 +0200

    thanks page added

---
+++ pages/thanks.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="thanks-wrap">
+    <b-container>
+      <div class="thanks flex-center text-center">
+        <Title
+          medium
+          bold
+          sky
+          title="شكرا لتواصلك معنا سيتم التواصل معك من فريق خدمة العملاء لتأكيد الحجز"
+        />
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Title from "../components/Title";
+
+export default {
+  name: "Thanks",
+
+  components: {
+    Title,
+  },
+};
+</script>
+
+<style scoped>
+.thanks {
+  height: 100vh;
+}
+</style>
+
